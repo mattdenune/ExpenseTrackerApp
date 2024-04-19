81f6866156f12c711490d5135a954ef2d6ac0e53
Matthew Denune <mattdenune@gmail.com>
2024-04-19 06:55:20 -0400

    small updates to ExpenseItem component

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -4,16 +4,20 @@ import React from "react";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
-export default function ExpenseItem({description, amount, date}) {
-
-	function expensePressHandler() {}
-
+export default function ExpenseItem({ description, amount, date }) {
+  function expensePressHandler() {}
 
   return (
-    <Pressable onPress={expensePressHandler}>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+      android_ripple={true}
+    >
       <View style={styles.expenseItem}>
         <View>
-          <Text style={[styles.textBase, styles.description]}>{description}</Text>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
           <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
@@ -25,38 +29,41 @@ export default function ExpenseItem({description, amount, date}) {
 }
 
 const styles = StyleSheet.create({
-	expenseItem: {
-		padding: 12,
-		marginVertical: 8,
-		backgroundColor: GlobalStyles.colors.primary500,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		borderRadius: 6,
-		elevation: 3,
-		shadowColor: GlobalStyles.colors.gray500,
-		shadowRadius: 4,
-		shadowOffset: {width: 1, height: 1},
-		shadowOpacity: 0.4
-	},
-	textBase: {
-		color: GlobalStyles.colors.primary50,
-	},
-	description: {
-		fontSize: 16,
-		marginBottom: 4,
-		fontWeight: 'bold',
-	},
-	amountContainer: {
-		paddingHorizontal: 12,
-		paddingVertical: 4,
-		backgroundColor: 'white',
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 4,
-		minWidth: 80,
-	},
-	amount: {
-		color: GlobalStyles.colors.primary500,
-		fontWeight: 'bold'
-	}
+  pressed: {
+    opacity: 0.75,
+  },
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    minWidth: 80,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+  },
 });
